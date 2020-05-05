de6432b355502110c17782da9103ee3261da16c7
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2020-05-04 22:18:11 -0300

    sessão01-aula05 - Estrutura das páginas - Página de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { FiLock, FiLogIn, FiMail } from 'react-icons/fi'
+import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -9,37 +9,36 @@ import Input from '../../components/Input'
 
 import { Background, Container, Content } from './styles'
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
+
     <Content>
       <img src={logoImg} alt="GoBarber" />
 
       <form>
-        <h1>Faça seu logon</h1>
+        <h1>Faça seu cadastro</h1>
 
+        <Input name="name" placeholder="Nome" Icon={FiUser} />
         <Input name="email" placeholder="E-mail" Icon={FiMail} />
 
         <Input
           name="password"
           type="password"
           placeholder="Senha"
-          autoComplete="current-password"
+          autoComplete="new-password"
           Icon={FiLock}
         />
 
-        <Button type="submit">Entrar</Button>
-
-        <a href="forgot">Esqueci minha senha</a>
+        <Button type="submit">Cadastrar</Button>
       </form>
 
-      <a href="/signup">
-        <FiLogIn />
-        Criar conta
+      <a href="/">
+        <FiArrowLeft />
+        Voltar para logon
       </a>
     </Content>
-
-    <Background />
   </Container>
 )
 
-export default SignIn
+export default SignUp
